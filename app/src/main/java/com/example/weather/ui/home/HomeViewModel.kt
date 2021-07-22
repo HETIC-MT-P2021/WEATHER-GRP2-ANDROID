66d373eab6b64d4444cee343fe53b51e10f89c3d
Valentin <36480710+valmrt77@users.noreply.github.com>
2021-07-22 14:28:05 +0200

    fix: screens

--- app/src/main/java/com/example/weather/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/weather/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "IL FAIT BO"
     }
     val text: LiveData<String> = _text
 }
